206478a2d985445df4dedd4a41e2d3208b8dcf62
mayur0977 <mayur0977@users.noreply.github.com>
2021-05-11 09:13:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import './app.styles.scss'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>ITS WORKING fine!</p>
+      </div>
+    )
+  }
+}
+
+export default App
